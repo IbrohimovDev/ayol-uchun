d1edf0f88428b7351ab3fadcaf6ec2e6e40e66ab
Muhammadyusuf <mrimofficial7@gmail.com>
2022-03-12 08:19:08 +0500

    translate added, but not tested

---
+++ lang.js
@@ -0,0 +1,191 @@
+const langArr = {
+    'title' : {
+        "uz" :  "Ayol uchun",
+        "en" : "For woman",
+        "ru" : "Для женщины",
+    },
+    'courses' : {
+        "uz" : "Kurslar",
+        "en" : "Курсы",
+        "en" : "",
+    },
+    'Ccourses' : {
+        "uz" : "KURSLAR",
+        "en" : "COURSES",
+        "ru" : "КУРСЫ",
+    },
+    'aboutProject' : {
+        "uz" : "Loyiha haqida",
+        "en" : "About Project",
+        "ru" : "О проекте",
+    },
+    'AaboutProject' : {
+        "uz" : "LOYIHA HAQIDA",
+        "en" : "ABOUT PROJECT",
+        "ru" : "О ПРОЕКТЕ",
+    },
+    'contact' : {
+        "uz": "Aloqa",
+        "en": "Contact",
+        "ru": "Контакт",
+    },
+    'login' : {
+        "uz" : "Kirish",
+        "en" : "Log in",
+        "ru" : "логин",
+    },
+    'mainTitle' : {
+        "uz" : "Foydali kurslar keng tanlovi",
+        "en" : "Wide selection of useful courses",
+        "ru" : "Широкий выбор полезных курсов",
+    },
+    "mainDesc" : {
+        "uz" : "Hozirgi va kelajakdagi ko'nikmalaringiz uchun. Biz bilan boshlang,",
+        "en" : "For your current and future skills. Start with us,",
+        "ru" : "Для ваших текущих и будущих навыков. Начните с нас,",
+    },
+    "growwithus" : {
+        "uz" : "biz bilan rivojlaning",
+        "ru" : "расти вместе с нами",
+        "en" : "grow with us",
+    },
+    "countupVideo" : {
+        "uz" : "Video kurslar",
+        "en" : "Video courses",
+        "ru" : "Видеокурсы",
+    },
+    "countupSpecialist" : {
+        "uz" : "Soha mutahasislari",
+        "en" : "Field specialists",
+        "ru" : "Полевые специалисты",
+    },
+    "coutnupPath" : {
+        "uz" : "Yo'nalishlar",
+        "en" : "Directions",
+        "ru" : "Направления",
+    },
+    "countupStudents" :{
+        "uz" : "O'quvchilar",
+        "en" : "Students",
+        "ru" : "Студенты",
+    },
+    "coursesInfoBestSeller" :{
+        "uz" : "Best-seller",
+        "en" : "Best-seller",
+        "ru" : "Бестселлер",
+    },
+    "coursesTitleDaily" :{
+        "uz" : "Kunlik taom tayyorlash bo‘yicha boshlang‘ich kurs",
+        "en" : "Daily cooking course",
+        "ru" : "Ежедневный кулинарный курс",
+    },
+    "btnWatch" : {
+        "uz" : "Videoni ko'rish",
+        "en" : "Watch a video",
+        "ru" : "Смотреть видео",
+    },
+    "coursesRecommend" : {
+        "uz" : "Tavsiya etiladi",
+        "en" : "Recommend",
+        "ru" : "Рекомендовать",
+    },
+    "coursesTitleMakeup": {
+        "uz" : "Pardoz bo‘yicha Liliya Rahimovadan master klasslar",
+        "en" : "Masterclass abour make-up from Liliya Rakhimova",
+        "ru" : "Мастер-класс по макияжу от Лилии Рахимовой"
+    },
+    "coursesShelf": {
+        "uz" : "Kiyim javoningizni yorqin ranglar bilan boyiting",
+        "en" : "Enrich your wardrobe with bright colors",
+        "ru" : "Обогатите свой гардероб яркими цветами"
+    },
+    "coursesPyscholog":{
+        "uz" : "Oilaviy psixologiya asoslari, boshlang‘ich tavsiyalar",
+        "en" : "Basics of family psychology, basic recommendations",
+        "ru" : "Основы семейной психологии, основные рекомендации",
+    },
+    "categoryTitle" : {
+        "uz" : "KATEGORIYALAR",
+        "en" : "CATEGORIES",
+        "ru" : "КАТЕГОРИИ"
+    },
+    "categoryPsychology":{
+        "uz" : "PSIXOLOGIYA",
+        "en" : "PSYCHOLOGY",
+        "ru" : "ПСИХОЛОГИЯ"
+    },
+    "categoryVijazist":{
+        "uz" : "VIZAJIST",
+        "en" : "VISAGISTE",
+        "ru" : "ВИЗАЖИСТ"
+    },
+    "categories14lesson" :{
+        "uz" : "Ja'mi 14ta dars",
+        "en" : "Total 14 lessons",
+        "ru" : "Всего 14 уроков"
+    },
+    "categoryCanditionary" :{
+        "uz" : "QANDOLATCHILIK",
+        "en" : "CONFECTIONARY",
+        "ru" : "КОНДИТЕРСКИЕ ИЗДЕЛИЯ"
+    },
+    "categoryDesign" : {
+        "uz" : "DIZAYN ASOSLARI",
+        "en" : "BASICS OF DESIGN",
+        "ru" : "ОСНОВЫ ДИЗАЙНА"
+    },
+    "categoryPediator":{
+        "uz" : "PEDIATOR",
+        "en" : "PEDIATRICIAN",
+        "ru" : "ПЕДИАТР"
+    },
+    "categoryGenikolog" :{
+        "uz" : "GINEKOLOGIYA",
+        "en" : "GYNECOLOGY",
+        "ru" : "ГИНЕКОЛОГИЯ"
+    },
+    "categoryWomanSpec" :{
+        "uz" : "AYOLLAR MUTAHASISI",
+        "en" : "SPECIALIST OF WOMEN",
+        "ru" : "СПЕЦИАЛИСТ ПО ЖЕНЩИНАМ"
+    },
+    "blogs":{
+        "uz" : "BLOGLAR",
+        "en" : "BLOGS",
+        "ru" : "БЛОГИ"
+    },
+    "date" : {
+        "uz" : "15 yanvar, 2002, 15:00",
+        "en" : "15 january, 2002, 15:00",
+        "ru" : "15 январ, 2002, 15:00",
+    },
+    "cleaning" : {
+        "uz" : "Ozodalik juda muhim",
+        "en" : "Cleanliness is very important",
+        "ru" : "Чистота очень важна"
+    },
+    "more" :{
+        "uz" : "Batafsil",
+        "en" : "Read more",
+        "ru" : "Подробнее"
+    },
+    "heyTitle": {
+        "uz": "👋🏻 Hey, Siz ham ayollar uchun foydali videolar tayyorlay olasizmi? Unda jamoamizga qo‘shiling!",
+        "en" : "👋🏻 Hey, can you make useful videos for women too? Then join our team! ",
+        "ru" : "👋🏻 Xэй, а можешь делать полезные видео и для женщин? Тогда присоединяйся к нашей команде!"
+    },
+    "btnAdd":{
+        "uz" : "Qo'shilish",
+        "en" : "Join",
+        "ru" : "Присоединиться"
+    },
+    "downApp": {
+        "uz" : "Ilovani yuklab oling va video kurslarni",
+        "en" : "Download app and watch video courses",
+        "ru" : "Скачайте приложение и смотрите видеокурсы"
+    },
+    "appWatch" : {
+        "uz" : ""
+    }
+
+}
--- script.js
+++ script.js
@@ -62,3 +62,29 @@ counters.forEach(counter=>{
   updateCount()
 })
 
+let select = document.querySelector('select')
+
+function changeURLLanguage(){
+  let lang = select.value
+  location.href = window.location.pathname + '#' + lang
+  location.reload()
+}
+
+function changeLanguage(){
+  let hash = window.location.hash
+  hash = hash.substr(1)
+  if(!allLang.includes(hash)){
+    location.href = window.location.pathname + '#en'
+    location.reload()
+  }
+  select.value = hash
+  document.querySelector('title').innerHTML = langArr['unit'][hash]
+  for(let key in langArr){
+    let elem = document.querySelector('.lng-' + key)
+    if(elem){
+      elem.innerHTML = langArr[key][hash]
+    }
+  }
+
+}
+changeLanguage()
